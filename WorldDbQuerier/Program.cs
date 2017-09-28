73ae164cd97f39e4ab09c8cb230ca6ddb66bde88
Karam Khalil <karam.khalil@immalle.eu>
2017-09-28 09:28:04 +0200

    WorldDbQuerier 0.1

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -4,9 +4,26 @@ namespace WorldDbQuerier
 {
     class Program
     {
+        static string version = "0.1";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-v":
+                        Console.WriteLine("versie {0}", version);
+                        break;
+                    default:
+                        Console.WriteLine("onbekende argument");
+                        break;                           
+                }
+            }
+            else
+            {
+                Console.WriteLine("hello world");
+            }
         }
     }
 }
