bd42d6a4ac951e534d6cf0bc2cbdce08c81ca679
Karam Khalil <karam.khalil@immalle.eu>
2017-10-26 10:04:29 +0200

    v 0.3

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -6,6 +6,7 @@ namespace WorldDbQuerier
     class Program
     {
         static string version = "0.3";
+        static int exit = 0;
 
         static void Main(string[] args)
         {
@@ -25,7 +26,47 @@ namespace WorldDbQuerier
             {
                 Console.WriteLine("hello world");
             }
+            while(exit == 0)
+            {
+                Console.WriteLine("Wat wilt u zien?");
+                Console.WriteLine("1. Het aantal landen aanwezig in de database.");
+                Console.WriteLine("2. Een lijst met alle landen aanwezig in de database.");
+                Console.WriteLine("3. exit");
 
+                switch(Console.ReadLine())
+                {
+                    case "1":
+                        Count();
+                        break;
+                    case "2":
+                        Readr();
+                        break;
+                    case "3":
+                        exit = 1;
+                        break;
+
+                }
+            }
+           
+            //Console.WriteLine("Wat wilt u zien?");
+            //Console.WriteLine("1. Het aantal landen aanwezig in de database.");
+            //Console.WriteLine("2. Een lijst met alle landen aanwezig in de database.");
+            //string keuze = Console.ReadLine();
+            //if (keuze == "1")
+            //{
+            //    Count();
+            //}
+            //else if(keuze == "2")
+            //{
+            //    Readr();
+            //}
+
+            
+
+        }
+
+        static void Count()
+        {
             MySqlConnection con = new MySqlConnection();
             con.ConnectionString = "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=immapwd;";
 
@@ -33,60 +74,35 @@ namespace WorldDbQuerier
             cmd1.Connection = con;
             cmd1.CommandText = "SELECT count(Country.name) from world.Country";
 
-            
-            //cmd2.Connection = con;
-            //cmd2.CommandText = "SELECT * FROM world.country";
-            //MySqlDataReader rdr = cmd2.ExecuteReader();
-            //while (rdr.Read())
-            //{
-            //    string city = (string)rdr["name"];
 
 
-            //    Console.WriteLine(city);
-            //}
 
             con.Open();
-            //cmd.ExecuteScalar();
-
-            
 
 
-            Console.WriteLine("Wat wilt u zien?");
-            Console.WriteLine("1. Het aantal landen aanwezig in de database.");
-            Console.WriteLine("2. Een lijst met alle landen aanwezig in de database.");
-            string keuze = Console.ReadLine();
-            if (keuze == "1")
-            {
-                Console.WriteLine(Convert.ToInt32(cmd1.ExecuteScalar()));
-            }
-            else if(keuze == "2")
-            {
-                SimpleRead();
-            }
-
-            
-
+            Console.WriteLine(Convert.ToString(cmd1.ExecuteScalar()));
         }
 
-        public static void SimpleRead()
+        static void Readr()
         {
-            MySqlDataReader rdr = null;
+            MySqlConnection conn = new MySqlConnection();
 
-            MySqlConnection con = new MySqlConnection();
-            con.ConnectionString = "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=immapwd;";
+            conn.ConnectionString = "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=immapwd;";
 
-            MySqlCommand cmd2 = new MySqlCommand("SELECT * FROM world.country");
+            conn.Open();
 
-            con.Open();
+            MySqlCommand cmd2 = new MySqlCommand();
+            cmd2.Connection = conn;
+            cmd2.CommandText = "select * from world.Country";
 
-            rdr = cmd2.ExecuteReader();
+            MySqlDataReader rdr = cmd2.ExecuteReader();
 
             while (rdr.Read())
             {
-                string city = (string)rdr["city"];
-
-                Console.WriteLine(city);
+                Console.WriteLine(rdr[1]);
             }
         }
+
+        }
     }
-}
+
